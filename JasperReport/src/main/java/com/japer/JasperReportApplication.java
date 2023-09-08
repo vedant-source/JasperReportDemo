02c21bd3eab5f3ce86f866b4bc4de3147b164663
vedantp <vedantp@cdac.in>
2023-09-08 14:45:55 +0530

    Jasper Report Tut Started ...

---
+++ JasperReport/src/main/java/com/japer/JasperReportApplication.java
@@ -0,0 +1,14 @@
+package com.japer;
+
+public class JasperReportApplication {
+
+	public static void main(String[] args) {
+	
+		try {
+			
+		} catch (Exception e) {
+			System.out.println("Exception While Creating Jasper Report ..!!!");
+		}
+	}
+
+}
